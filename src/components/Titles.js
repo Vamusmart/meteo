b0f131635d3832a5b14d3015f2b6a3e76fe99e72
Administrator <administrator@CN-4.lan>
2019-03-19 19:33:56 +0000

    changes

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component{
     render(){
         return(
             <div>
-                <h1>Meteo/Weather</h1>
-                <p>Com'e' il tempo oggi?/Is it a beautiful day?</p>
+                <h1 className="title-container__title">Weather</h1>
+                <h3 className="title-container__subtitle">Is it sunny Today?</h3>
             </div>
         );
     }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,17 +4,34 @@ class Weather extends React.Component {
   render() {
     return (
       //Display on the screen the props after we click the button getWeather. Data passes from the consoleLog to the display
-      <div>
+      <div className="weather__info">
         {/* && condition operator for react, that shows the text below(Location, Temp, Hum etc) just when we click the button getWeather: when the value of city && country && temp etc are true, so they contain a value */}
         {this.props.city && this.props.country && (
-          <p>
-            Location: {this.props.city}, {this.props.country}
+          <p className="weather__key">
+            Location:
+            <span className="weather__value"> {this.props.city}, {this.props.country}
+            </span>
           </p>
         )}
-        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-        {this.props.description && <p>Conditions: {this.props.description}</p>}
-        {this.props.error && <p>{this.props.error}</p>}
+        {this.props.temperature && (
+          <p className="weather__key">
+            Temperature:
+            <span className="weather__value"> {this.props.temperature}</span>
+          </p>
+        )}
+        {this.props.humidity && (
+          <p className="weather__key">
+            Humidity:
+            <span className="weather__value"> {this.props.humidity}</span>
+          </p>
+        )}
+        {this.props.description && (
+          <p className="weather__key">
+            Conditions:
+            <span className="weather__value"> {this.props.description}</span>
+          </p>
+        )}
+        {this.props.error && <p className="weather__error">{this.props.error}</p>}
       </div>
     );
   }
